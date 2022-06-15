f3c0d51bc899874518fe861ca99d90c0f30b588d
brianmwevi <mwevibrian@gmail.com>
2022-06-15 10:03:16 +0300

    ft:user serializer -- add create method using knox

--- api/serializers.py
+++ api/serializers.py
@@ -13,4 +13,11 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
-   
+    def create(self, validated_data):
+        user = User(email=validated_data['email'],
+                    username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+        # Token.objects.create(user=user)
+        return user
+
