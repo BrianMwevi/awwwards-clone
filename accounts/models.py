9d08d6e0806e084a247225fa2de333c27c94cd87
brianmwevi <mwevibrian@gmail.com>
2022-06-15 09:44:35 +0300

    ft:profile -- add create profile signal upon signup

--- accounts/models.py
+++ accounts/models.py
@@ -14,7 +14,13 @@ class Profile(models.Model):
     is_jury = models.BooleanField(default=False)
     image = models.ImageField(upload_to='images/', default="profile.png")
 
- 
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+  
            
     def __str__(self):
         return self.user.username
