a79b808496f55e07640332c1d67f33bb286315d3
brianmwevi <mwevibrian@gmail.com>
2022-06-15 09:54:15 +0300

    ft:label -- add label model

--- projects/models.py
+++ projects/models.py
@@ -62,3 +62,14 @@ def update_rating(sender, instance, created, **kwargs):
         project.ratings.add(instance)
         project.save()
         instance.save()
+
+
+class Label(models.Model):
+    name = models.CharField(max_length=25)
+    projects = models.ManyToManyField(
+        Project, related_name="label", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
