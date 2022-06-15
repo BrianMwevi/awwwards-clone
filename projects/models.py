f217c2407520693abfcfd8b0fba49c6db0efdbbe
brianmwevi <mwevibrian@gmail.com>
2022-06-15 09:51:36 +0300

    ft:project -- add project model

---
+++ projects/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+
+
+class Project(models.Model):
+    user = models.ForeignKey(
+        User, related_name='author', on_delete=models.CASCADE)
+    name = models.CharField(max_length=55)
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField()
+    ratings = models.ManyToManyField(
+        'Rating', related_name='user_ratings', blank=True)
+    design = models.PositiveBigIntegerField(blank=True, default=0)
+    creativity = models.PositiveBigIntegerField(blank=True, default=0)
+    content = models.PositiveBigIntegerField(blank=True, default=0)
+    usability = models.PositiveBigIntegerField(blank=True, default=0)
+    average_rating = models.PositiveBigIntegerField(
+        blank=True, default=0)
+    labels = models.ManyToManyField(
+        "Label", related_name="project", blank=True)
+    posted_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-posted_at',)
+
+    def __str__(self):
+        return self.name
