7923a306168ba5490899013cfcecd456c7863d33
brianmwevi <mwevibrian@gmail.com>
2022-06-15 10:10:18 +0300

    ft:user signup -- add user creation view and view post method

--- api/views.py
+++ api/views.py
@@ -40,3 +40,17 @@ class ProfileViewset(generics.RetrieveUpdateDestroyAPIView):
         return Response(data, status=status.HTTP_200_OK)
     # TODO: Add permission to only request own's profile
 
+
+class UserCreate(generics.CreateAPIView):
+    authentication_classes = ()
+    permission_classes = ()
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            # "token": AuthToken.objects.create(user)[1]
+        })
